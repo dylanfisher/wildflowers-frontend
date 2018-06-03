b284706ff535fd7be5d7b50dab7c6277bea27291
dylanfisher <hi@dylanfisher.com>
2018-06-03 17:33:33 -0400

    Add drawing canvas

---
+++ src/components/DrawingCanvas.vue
@@ -0,0 +1,44 @@
+<template>
+  <canvas id="drawing-canvas"></canvas>
+</template>
+
+<script>
+import SignaturePad from 'signature_pad'
+
+export default {
+  name: 'DrawingCanvas',
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      /* eslint-disable */
+      const signaturePad = new SignaturePad(this.$el, {
+        velocityFilterWeight: 0.7,
+        minWidth: 0.5,
+        maxWidth: 2.5,
+        throttle: 0,
+        minDistance: 5
+      })
+
+      const ratio = Math.max(window.devicePixelRatio || 1, 1)
+
+      this.$el.width = this.$el.offsetWidth * ratio;
+      this.$el.height = this.$el.offsetHeight * ratio;
+      this.$el.getContext('2d').scale(ratio, ratio);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #drawing-canvas {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <drawing-canvas/>
+  </div>
+</template>
+
+<script>
+import DrawingCanvas from './DrawingCanvas.vue'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    DrawingCanvas
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
